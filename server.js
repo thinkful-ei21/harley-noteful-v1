08ee6b3d2391964b5b46096744f1ad8b260c8b34
harleyjj <harleyyesm@gmail.com>
2018-05-22 13:41:29 -0700

    updated get /api/notes/:id to use simdb find function

--- server.js
+++ server.js
@@ -28,8 +28,15 @@ app.get('/api/notes',(req, res, next) => {
   });
 });
 
-app.get('/api/notes/:id',(req, res) => {
-  res.json(data.find(item => item.id === Number(req.params.id)));
+app.get('/api/notes/:id',(req, res, next) => {
+  const { id } = req.params;
+
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    res.json(item);
+  });
 });
 
 app.use(function (req, res, next) {
